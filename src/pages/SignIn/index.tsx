cd01f12229e4e2dc3feaa6be64b742b6cad448ba
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-09-27 19:19:57 -0300

    styles with keyboard open

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -7,6 +7,7 @@ import {
   ScrollView,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -23,6 +24,7 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <>
       <KeyboardAvoidingView
@@ -65,7 +67,7 @@ const SignIn: React.FC = () => {
 
       <CreateAccountButton
         onPress={() => {
-          console.log('pressed');
+          navigation.navigate('SignUp');
         }}
       >
         <Icon name="log-in" size={20} color="#ff9000" />
